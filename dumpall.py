5496601823c79ede082b8fa05ca33e16dda87909
EgorBron <bataikinegor@yandex.ru>
2022-06-09 12:43:23 +0300

    Code moved to diSCUM

--- dumpall.py
+++ dumpall.py
@@ -1,16 +1,17 @@
-import discord
-
-me = discord.Client()
-
-@me.event
-async def on_message(msg):
-    if msg.content.lower().strip() == 'dumpall':
-       ... # we will create some magic soon
-
-@me.event
-async def on_ready():
-    print('Connected!')
-
 with open('token.txt') as tkf:
     tk = tkf.readline()
-me.run(tk) 
+
+import discum     
+me = discum.Client(token=tk, log=False)
+
+@bot.gateway.command
+def evt(resp):
+    if resp.event.ready_supplemental: #ready_supplemental is sent after ready
+        user = me.gateway.session.user
+        print("Logged in as {}#{}".format(user['username'], user['discriminator']))
+    if resp.event.message:
+        msg = resp.parsed.auto()
+        if msg['content'].lower().strip() == 'dumpall':
+            ... # we will create some magic soon
+
+me.gateway.run(auto_reconnect=True)
